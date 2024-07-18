232a3182572dbe573d5bf3b61ee2392bc5ef06fb
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-07-18 12:41:04 -0700

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,6 @@
 
-export async function GET(request) {
-  console.log('Received', request.json())
+export async function POST(request) {
+  console.log('Received', request.text())
 
   return new Response('ack', {
     status: 200,
